90b57b2807e1f2010edd59a08a14d03da450d6c3
dauxuanhoanghung <dxhh2510@gmail.com>
2024-01-31 17:41:12 +0700

    fix error about remove unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductModule, DatabaseModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
