64c35596df50b964b60c8b61d0f0353a075b6f14
dungngoo <ngodung06vn@gmail.com>
2024-04-05 09:17:57 +0700

    Create CRUD BE (News)

--- src/modules/news/news.controller.ts
+++ src/modules/news/news.controller.ts
@@ -13,18 +13,18 @@ import {
 import { News } from 'src/entities/News.entity';
 import { NewsService } from './News.service';
 
-@Controller('News')
+@Controller('news')
 export class NewsController {
-  constructor(private Newservice: NewsService) {}
+  constructor(private newsService: NewsService) {}
 
-  //Ex: http://localhost:5000/News - GET
+  //Ex: http://localhost:5000/news - GET
   @Get()
   async getAll(): Promise<News[]> {
-    return this.Newservice.findAll();
+    return this.newsService.findAll();
   }
 
-  //Ex: http://localhost:5000/News - POST
-  @Post()
+  //Ex: http://localhost:5000/news - POST
+  @Post('/create')
   async create(
     @Body() data: any,
     @UploadedFiles()
@@ -33,16 +33,16 @@ export class NewsController {
       images?: Express.Multer.File[];
     },
   ): Promise<any> {
-    return this.Newservice.create(data, files);
+    return this.newsService.create(data, files);
   }
 
-  //Ex: http://localhost:5000/News/${id} - GET
+  //Ex: http://localhost:5000/news/${id} - GET
   @Get(':id')
-  async getById(@Param('id') id: string): Promise<News> {
-    return this.Newservice.findById(id);
+  async getById(@Param('id') id: any): Promise<News> {
+    return this.newsService.findById(id);
   }
 
-  //Ex: http://localhost:5000/News/${id} - PUT
+  //Ex: http://localhost:5000/news/${id} - PUT
   @Put(':id')
   async update(
     @Param('id') id: string,
@@ -52,12 +52,12 @@ export class NewsController {
       image?: Express.Multer.File[];
     },
   ): Promise<News> {
-    return this.Newservice.update(id, data, files);
+    return this.newsService.update(id, data, files);
   }
 
-  //Ex: http://localhost:5000/News/${id} - DELETE
+  //Ex: http://localhost:5000/news/${id} - DELETE
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
-    return this.Newservice.remove(id);
+    return this.newsService.remove(id);
   }
 }
--- src/repositories/news.repository.ts
+++ src/repositories/news.repository.ts
@@ -7,46 +7,46 @@ import { News } from 'src/entities/news.entity';
 
 @Injectable()
 export class NewsRepository {
-  constructor(@InjectModel(News.name) private NewsModel: Model<News>) {}
+  constructor(@InjectModel(News.name) private newsModel: Model<News>) {}
 
   async findAll(): Promise<News[]> {
-    return this.NewsModel.find().exec();
+    return this.newsModel.find().exec();
   }
 
   async create(data: News): Promise<News> {
-    const newEmmloyee = new this.NewsModel(data);
-    return newEmmloyee.save();
+    const createdNews = new this.newsModel(data);
+    return createdNews.save();
   }
 
   async findById(id: string): Promise<News> {
-    return this.NewsModel.findById(id).exec();
+    return this.newsModel.findById(id).exec();
   }
 
   async update(id: string, data: News): Promise<News> {
-    const News = await this.NewsModel.findById(id);
+    const news = await this.newsModel.findById(id);
 
-    if (!News) {
+    if (!news) {
       return null;
     }
 
     Object.keys(data).forEach((key) => {
       if (data[key] !== undefined) {
-        News[key] = data[key];
+        news[key] = data[key];
       }
     });
 
-    const updatedNews = await News.save();
+    const updatedNews = await news.save();
     return updatedNews;
   }
 
   async remove(id: string): Promise<{ status: number; message: string }> {
     try {
-      const news = await this.NewsModel.findById(id);
+      const news = await this.newsModel.findById(id);
       if (!news) {
         return { status: 404, message: 'News not found' };
       }
 
-      const result = await this.NewsModel.deleteOne({ _id: id });
+      const result = await this.newsModel.deleteOne({ _id: id });
       if (result.deletedCount > 0) {
         return { status: 204, message: 'News deleted successfully' };
       } else {
