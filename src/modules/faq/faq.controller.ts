4e57d1fc5176b00af8bbd279c5b58509c34968aa
2051052034hai <2051052034hai@ou.edu.vn>
2024-04-17 23:42:52 +0700

    handel api 17/04/2024

--- src/modules/faq/faq.controller.ts
+++ src/modules/faq/faq.controller.ts
@@ -7,42 +7,48 @@ import {
   Param,
   Post,
   Put,
+  Query,
+  UsePipes,
 } from '@nestjs/common';
 
 import { Faq } from 'src/entities/Faq.entity';
 import { FaqService } from './Faq.service';
+import { QueryParserPipe } from 'src/query/query-parser.pipe';
 
 @Controller('Faq')
 export class FaqController {
-  constructor(private Faqervice: FaqService) {}
+  constructor(private FaqService: FaqService) {}
 
   //Ex: http://localhost:5000/Faq - GET
   @Get()
-  async getAll(): Promise<Faq[]> {
-    return this.Faqervice.findAll();
+  @UsePipes(QueryParserPipe)
+  async getAll(
+    @Query() query?: string,
+  ): Promise<{ data: Faq[]; total: number }> {
+    return this.FaqService.findAll(query);
   }
 
   //Ex: http://localhost:5000/Faq - POST
   @Post()
   async create(@Body() data: any): Promise<any> {
-    return this.Faqervice.create(data);
+    return this.FaqService.create(data);
   }
 
   //Ex: http://localhost:5000/Faq/${id} - GET
   @Get(':id')
   async getById(@Param('id') id: string): Promise<Faq> {
-    return this.Faqervice.findById(id);
+    return this.FaqService.findById(id);
   }
 
   //Ex: http://localhost:5000/Faq/${id} - PUT
   @Put(':id')
   async update(@Param('id') id: string, @Body() data: any): Promise<Faq> {
-    return this.Faqervice.update(id, data);
+    return this.FaqService.update(id, data);
   }
 
   //Ex: http://localhost:5000/Faq/${id} - DELETE
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
-    return this.Faqervice.remove(id);
+    return this.FaqService.remove(id);
   }
 }
